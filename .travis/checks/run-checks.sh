eda82fbfececb0b62d0460fd79324c987185e734
Dawid Drozd <dawid.drozd@arm.com>
2019-01-21 10:26:27 +0000

    Convert tabs to spaces
    Change-Id: I587c0235906ba395c375c990c1e23aca6537f440
    Signed-off-by: Dawid Drozd <dawid.drozd@arm.com>

--- .travis/checks/run-checks.sh
+++ .travis/checks/run-checks.sh
@@ -4,24 +4,24 @@ STATUS_CODE=0 # reset
 
 ####################
 fold_start 'Check:go-vet'
-	bash .travis/checks/check-go-vet.sh
-	check_result $? "Check 'go vet': "
+    bash .travis/checks/check-go-vet.sh
+    check_result $? "Check 'go vet': "
 fold_end
 ####################
 
 ####################
 fold_start 'Check:gofmt'
-	bash .travis/checks/check-code-format.sh
-	check_result $? "Check gofmt: "
+    bash .travis/checks/check-code-format.sh
+    check_result $? "Check gofmt: "
 fold_end
 ####################
 
 ####################
 fold_start 'Check:signoff'
-	git log  --pretty=oneline | head -n 10 # Very useful for debug we should keep this
-	echo "----"
-	python3 .travis/checks/check-signoff.py
-	check_result $? "Check signoff: "
+    git log  --pretty=oneline | head -n 10 # Very useful for debug we should keep this
+    echo "----"
+    python3 .travis/checks/check-signoff.py
+    check_result $? "Check signoff: "
 fold_end
 ####################
 
--- .travis/validate_format.sh
+++ .travis/validate_format.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -n "$(gofmt -l .)" ]; then
-	echo "Go code is not formatted:"
-	echo "########################"
-	gofmt -d .
-	echo "########################"
-	exit 1
+    echo "Go code is not formatted:"
+    echo "########################"
+    gofmt -d .
+    echo "########################"
+    exit 1
 fi
