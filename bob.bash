56c7bd66c5e7c355ff1e057ff88d1d7c3c38bb59
Chris Diamand <chris.diamand@arm.com>
2019-02-28 14:14:47 +0000

    Use Android's prebuilt Ninja
    Include `ninja_config.mk`, which sets up the NINJA environment variable,
    which contains the location of Ninja within Android's prebuilts.
    Propagate this through the bootstrap process so that it is used when
    building.
    
    When NINJA is not set, default to using `ninja`, which will be found
    using PATH as usual.
    
    Change-Id: Ie9ccff0e08375ba91b5a6a70ae394fa3e886bf68
    Signed-off-by: Chris Diamand <chris.diamand@arm.com>

--- bob.bash
+++ bob.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2018 Arm Limited.
+# Copyright 2018-2019 Arm Limited.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -48,4 +48,4 @@ export CCACHE_SLOPPINESS=file_macro,time_macros
 BUILDDIR="${BUILDDIR}" SKIP_NINJA=true ${BOB_DIR}/blueprint/blueprint.bash
 
 # Do the actual build
-ninja -f "${BUILDDIR}/build.ninja" -w dupbuild=err "$@"
+"${NINJA}" -f "${BUILDDIR}/build.ninja" -w dupbuild=err "$@"
