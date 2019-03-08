43294b80600d6e2157a0355f2d6fa7aed7677cc5
Chris Diamand <chris.diamand@arm.com>
2019-03-08 17:02:21 +0000

    Support running multiple formatter tests
    Rather than hard-coding the input and expected output files, iterate
    through the `formatter` directory and find pairs of `.test` and
    `.expected` files which combine to make a formatter test case. This
    allows there to easily be multiple tests.
    
    Change-Id: I7e72eff0090896c060501afcff6a8f526038edd6
    Signed-off-by: Chris Diamand <chris.diamand@arm.com>

--- config_system/tests/run_tests_formatter.py
+++ config_system/tests/run_tests_formatter.py
@@ -24,32 +24,46 @@ BOB_DIR = os.path.dirname(os.path.dirname(TEST_DIR))
 sys.path.append(BOB_DIR)
 
 
-def run_fix_test(name, expected_output):
+def run_test(name, expected_output):
     """ Test function to verify difference between two file contents"""
     from config_system import mconfigfmt
+
+    passed = True
+
     print("Running %s" % name)
     tmp_file = tempfile.NamedTemporaryFile(mode="w+", delete=False)
-    tests_run = 0
-    tests_failed = 0
     mconfigfmt.perform_formatting(name, tmp_file.file)
     tmp_file.close()
+
     with open(tmp_file.name) as test_out, open(expected_output) as exp_out:
         out_lines, exp_lines = test_out.readlines(), exp_out.readlines()
     if any(out_lines[i] != exp_lines[i] for i in range(len(exp_lines))):
-        tests_failed += 1
-    tests_run += 1
+        passed = False
+
     os.remove(tmp_file.name)
-    return tests_run, tests_failed
+
+    return passed
 
 
 def main():
     formatter_tests = os.path.join(TEST_DIR, "formatter")
-    os.chdir(formatter_tests)
-    test_run, failed = run_fix_test("formatter.test", "expected.out")
+
+    tests_passed = 0
+    tests_failed = 0
+
+    for fname in os.listdir(formatter_tests):
+        base, ext = os.path.splitext(fname)
+        if ext == ".test":
+            test = os.path.join(formatter_tests, base)
+            passed = run_test(test + ".test", test + ".expected")
+            if passed:
+                tests_passed += 1
+            else:
+                tests_failed += 1
 
     print("")
-    print("{} tests run, {} failed".format(test_run, failed))
-    if failed > 0:
+    print("{} tests run, {} failed".format(tests_passed + tests_failed, tests_failed))
+    if tests_failed > 0:
         sys.exit(1)
 
 
