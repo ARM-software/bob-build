0e3252472a830711c61080e0a06f4189f4d18c53
David Kilroy <david.kilroy@arm.com>
2019-05-10 15:29:33 +0100

    Fix bob tests when work dir is the output dir
    Kernel module kernel_dir is assumed to be absolute or relative to
    source path.
    
    We can no longer test export_include_dirs on Linux.
    
    Add travis tests to check building in different locations.
    
    Change-Id: Ida0af224b01f9bf317aa280000d0eef2fc278041
    Signed-off-by: David Kilroy <david.kilroy@arm.com>

--- .travis/run_build_tests.sh
+++ .travis/run_build_tests.sh
@@ -3,10 +3,29 @@ set -eE
 trap "echo '<------------- run_build_tests.sh failed'" ERR
 
 export TEST_NON_ASCII_IN_ENV_HASH='ó'
-build_dir=build-test
-cd ${BOB_ROOT}/tests/
-rm -rf ${build_dir} # Cleanup test directory
-./bootstrap -o ${build_dir}
+
 # Test by explicitly requesting the `bob_tests` alias, which should include all
 # test cases, including alias tests, which can't just set `build_by_default`.
+
+# Build with working directory in source directory
+build_dir=build-in-src
+cd "${BOB_ROOT}/tests"
+rm -rf ${build_dir} # Cleanup test directory
+./bootstrap -o ${build_dir}
 ${build_dir}/config && ${build_dir}/buildme bob_tests
+
+# Build in an independent working directory
+build_dir=build-indep
+cd "${BOB_ROOT}"
+rm -rf ${build_dir} # Cleanup test directory
+tests/bootstrap -o ${build_dir}
+${build_dir}/config && ${build_dir}/buildme bob_tests
+
+# Build with the working directory in the output directory
+build_dir=build-in-outp
+cd "${BOB_ROOT}"
+rm -rf ${build_dir} # Cleanup test directory
+mkdir ${build_dir}
+cd ${build_dir}
+../tests/bootstrap -o .
+./config && ./buildme bob_tests
