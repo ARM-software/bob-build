3052d92571547a766ea7fd3fdd35cf78d5e08db9
David Kilroy <david.kilroy@arm.com>
2019-06-27 09:58:53 +0100

    Improve match_srcs test
    Setup the test so that it can verify the concatenation of the files
    works as expected, and require the content of both files to be
    present. Do this by compiling the output.
    
    Make the dynamic-list option actually do something by giving it a
    valid function to export. Rename the files to reflect what they would
    actually be doing in this test.
    
    Change-Id: I38e984264227cad7b4ed5ae042973eb8200226e9
    Signed-off-by: David Kilroy <david.kilroy@arm.com>

--- tests/match_source/main.c
+++ tests/match_source/main.c
@@ -1,6 +1,11 @@
-#include "main.h"
+// Expect to have the exported_function() definition
+// prepended to this file to test {{match_srcs}}
+
+extern int another_function(void);
 
 int main(void)
 {
-    return A == 42 ? 0 : 1;
+	int result = exported_function();
+	result += another_function();
+	return (result == 46) ? 0 : 1;
 }
