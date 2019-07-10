49301855262d34631b78a40f10fee7da8384ff9c
David Kilroy <david.kilroy@arm.com>
2019-07-10 17:40:18 +0100

    Do commit checks once
    Rather than on every combination of Go and python. Also allow Go and
    Python specific testing to be enabled and disabled.
    
    In the build matrix, do the commit checks with Python 3 and test
    python code once with python 2.7 and once with python 3. Test the Go
    code in all variants.
    
    Change-Id: I82cb2e5282dca18f28ed250b81e8f26993aa73b0
    Signed-off-by: David Kilroy <david.kilroy@arm.com>

--- .travis/run_all_tests.sh
+++ .travis/run_all_tests.sh
@@ -23,39 +23,45 @@ fold_start 'build_tests.sh'
 fold_end
 ####################
 
-####################
-fold_start 'run_go_tests.sh'
-    bash .travis/run_go_tests.sh
-    check_result $? "Go tests:"
-fold_end
-####################
+# Tests of go code (python version doesn't matter)
+if [ ${DO_GO_TESTS} -eq 1 ] ; then
+    ####################
+    fold_start 'run_go_tests.sh'
+        bash .travis/run_go_tests.sh
+        check_result $? "Go tests:"
+    fold_end
+    ####################
+fi
+
+# Tests of python code (go version doesn't matter)
+if [ ${DO_PYTHON_TESTS} -eq 1 ] ; then
+    ####################
+    fold_start 'run_tests.py'
+        config_system/tests/run_tests.py
+        check_result $? "config_system regression tests:"
+    fold_end
+    ####################
+
+    ####################
+    fold_start 'run_formatter_tests.py'
+        config_system/tests/run_tests_formatter.py
+        check_result $? "Mconfigfmt tests:"
+    fold_end
+    ####################
+
+    ####################
+    fold_start 'pytest config_system'
+        # The newer command `pytest` is not available on Ubuntu 16.04, which the
+        # Travis environment uses, so invoke the older `py.test` here.
+        py.test-${PYTHON_SUFFIX} config_system
+        check_result $? "config_system pytest:"
+    fold_end
+    ####################
+fi
 
 ####################
-fold_start 'run_tests.py'
-    config_system/tests/run_tests.py
-    check_result $? "config_system regression tests:"
-fold_end
-####################
-
-####################
-fold_start 'run_formatter_tests.py'
-    config_system/tests/run_tests_formatter.py
-    check_result $? "Mconfigfmt tests:"
-fold_end
-####################
-
-####################
-fold_start 'pytest config_system'
-    # The newer command `pytest` is not available on Ubuntu 16.04, which the
-    # Travis environment uses, so invoke the older `py.test` here.
-    py.test-${PYTHON_SUFFIX} config_system
-    check_result $? "config_system pytest:"
-fold_end
-####################
-
-# This test is issued only if build test passed previously
-####################
-
+# This test is issued only if build test passed previously.
+# We do this last as this test changes the checkout
 fold_start 'run_bootstrap_not_required.sh'
     if [[ ${build_result} == 0 ]];then
         bash .travis/run_bootstrap_test.sh
