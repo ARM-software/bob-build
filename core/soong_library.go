b4c95bf164d618323d95db98619eef55bf67694d
Chris Diamand <chris.diamand@arm.com>
2019-07-22 17:58:36 +0100

    Implement exported cflags
    Re-implement the getExportedVariables function for Soong (the current
    implementation uses a ModuleContext, which is not _quite_ compatible
    with TopDownMutatorContext, so cannot be re-used).
    
    Update the structure of the non-Soong version for consistency.
    
    Set ldflags and exported include dirs in the generated cc_library
    module. This means that most tests based on static libraries and
    binaries can be enabled for Soong.
    
    Change-Id: I0bd866b66ba77ebe41da057b66378f4e6fc6ac68
    Signed-off-by: Chris Diamand <chris.diamand@arm.com>

--- core/soong_library.go
+++ core/soong_library.go
@@ -19,6 +19,8 @@
 package core
 
 import (
+	"fmt"
+
 	"android/soong/android"
 	"android/soong/cc"
 
@@ -36,6 +38,33 @@ type ccLibraryCommonProps struct {
 	Local_include_dirs []string
 	Static_libs        []string
 	Whole_static_libs  []string
+	Ldflags            []string
+}
+
+type ccStaticOrSharedProps struct {
+	Export_include_dirs []string
+}
+
+func (l *library) getExportedCflags(mctx android.TopDownMutatorContext) []string {
+	visited := map[string]bool{}
+	cflags := []string{}
+	mctx.VisitDirectDeps(func(dep android.Module) {
+		if !(mctx.OtherModuleDependencyTag(dep) == wholeStaticDepTag ||
+			mctx.OtherModuleDependencyTag(dep) == staticDepTag ||
+			mctx.OtherModuleDependencyTag(dep) == sharedDepTag ||
+			mctx.OtherModuleDependencyTag(dep) == reexportLibsTag) {
+			return
+		} else if _, ok := visited[dep.Name()]; ok {
+			// VisitDirectDeps will visit a module once for each
+			// dependency. We've already done this module.
+			return
+		}
+
+		if sl, ok := getLibrary(dep); ok {
+			cflags = append(cflags, sl.Properties.Export_cflags...)
+		}
+	})
+	return cflags
 }
 
 func (l *library) setupCcLibraryProps(mctx android.TopDownMutatorContext) (bool, *ccLibraryCommonProps) {
@@ -43,39 +72,53 @@ func (l *library) setupCcLibraryProps(mctx android.TopDownMutatorContext) (bool,
 		return false, nil
 	}
 
+	if len(l.Properties.Export_include_dirs) > 0 {
+		panic(fmt.Errorf("Module %s exports non-local include dirs %v - this is not supported",
+			mctx.ModuleName(), l.Properties.Export_include_dirs))
+	}
+
 	// For now, only build target libraries
 	if l.Properties.TargetType != tgtTypeTarget {
 		return false, nil
 	}
 
+	cflags := utils.NewStringSlice(l.Properties.Cflags,
+		l.Properties.Export_cflags, l.getExportedCflags(mctx))
+
 	props := &ccLibraryCommonProps{
 		Name:               proptools.StringPtr(l.Name()),
 		Srcs:               utils.Filter(utils.IsCompilableSource, l.Properties.Srcs),
 		Exclude_srcs:       l.Properties.Exclude_srcs,
-		Cflags:             l.Properties.Cflags,
+		Cflags:             cflags,
 		Include_dirs:       l.Properties.Include_dirs,
 		Local_include_dirs: l.Properties.Local_include_dirs,
-		Static_libs:        utils.NewStringSlice(l.Properties.Static_libs, l.Properties.Export_static_libs),
+		Static_libs:        l.Properties.ResolvedStaticLibs,
 		Whole_static_libs:  l.Properties.Whole_static_libs,
+		Ldflags:            l.Properties.Ldflags,
 	}
 
 	return true, props
 }
 
 func (l *staticLibrary) soongBuildActions(mctx android.TopDownMutatorContext) {
-	enabled, props := l.setupCcLibraryProps(mctx)
+	enabled, commonProps := l.setupCcLibraryProps(mctx)
 	if !enabled {
 		return
 	}
 
-	mctx.CreateModule(android.ModuleFactoryAdaptor(cc.LibraryStaticFactory), props)
+	libProps := &ccStaticOrSharedProps{
+		// Soong's `export_include_dirs` field is relative to the module dir.
+		Export_include_dirs: l.Properties.Export_local_include_dirs,
+	}
+
+	mctx.CreateModule(android.ModuleFactoryAdaptor(cc.LibraryStaticFactory), commonProps, libProps)
 }
 
 func (b *binary) soongBuildActions(mctx android.TopDownMutatorContext) {
-	enabled, props := b.setupCcLibraryProps(mctx)
+	enabled, commonProps := b.setupCcLibraryProps(mctx)
 	if !enabled {
 		return
 	}
 
-	mctx.CreateModule(android.ModuleFactoryAdaptor(cc.BinaryFactory), props)
+	mctx.CreateModule(android.ModuleFactoryAdaptor(cc.BinaryFactory), commonProps)
 }
