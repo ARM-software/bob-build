0ea13e458c5e0586f2028ec3d904ec84f776bb39
Chris Diamand <chris.diamand@arm.com>
2019-07-24 16:27:50 +0100

    Build host libraries
    Enable building of host libraries. This is non-trivial, because the
    obvious solution (just changing the factories passed to `CreateModule`)
    would create multiple modules of the same name, which does not work.
    
    Work around that by using the library's `shortName`, which disambiguates
    between the host and target versions, then fixing up the generated
    filename using Soong's `stem` field.
    
    Disable the `generate_libs` tests; generated libraries do not
    currently work, because they don't set the variation correctly and
    therefore cannot be linked to. This is exposed now that we support host
    binaries, which are used in those tests.
    
    Change-Id: Iade3785877595559fc121bb44cce29ca8a11ebde
    Signed-off-by: Chris Diamand <chris.diamand@arm.com>

--- core/soong_library.go
+++ core/soong_library.go
@@ -31,6 +31,7 @@ import (
 
 type ccLibraryCommonProps struct {
 	Name               *string
+	Stem               *string
 	Srcs               []string
 	Exclude_srcs       []string
 	Cflags             []string
@@ -43,6 +44,32 @@ type ccLibraryCommonProps struct {
 
 type ccStaticOrSharedProps struct {
 	Export_include_dirs []string
+}
+
+// Convert between Bob module names, and the name we will give the generated
+// cc_library module. This is required when a module supports being built on
+// host and target; we cannot create two modules with the same name, so
+// instead, we use the `shortName()` (which may include a `__host` or
+// `__target` suffix) to disambiguate, and use the `stem` property to fix up
+// the output filename.
+func ccModuleName(mctx android.TopDownMutatorContext, name string) string {
+	m, _ := mctx.GetDirectDep(name + bobModuleSuffix)
+
+	if l, ok := getLibrary(m); ok {
+		return l.shortName()
+	}
+
+	return m.Name()
+}
+
+func ccModuleNames(mctx android.TopDownMutatorContext, nameLists ...[]string) []string {
+	ccModules := []string{}
+	for _, nameList := range nameLists {
+		for _, name := range nameList {
+			ccModules = append(ccModules, ccModuleName(mctx, name))
+		}
+	}
+	return ccModules
 }
 
 func (l *library) getExportedCflags(mctx android.TopDownMutatorContext) []string {
@@ -67,58 +94,84 @@ func (l *library) getExportedCflags(mctx android.TopDownMutatorContext) []string
 	return cflags
 }
 
-func (l *library) setupCcLibraryProps(mctx android.TopDownMutatorContext) (bool, *ccLibraryCommonProps) {
-	if !isEnabled(l) {
-		return false, nil
-	}
-
+func (l *library) setupCcLibraryProps(mctx android.TopDownMutatorContext) *ccLibraryCommonProps {
 	if len(l.Properties.Export_include_dirs) > 0 {
 		panic(fmt.Errorf("Module %s exports non-local include dirs %v - this is not supported",
 			mctx.ModuleName(), l.Properties.Export_include_dirs))
-	}
-
-	// For now, only build target libraries
-	if l.Properties.TargetType != tgtTypeTarget {
-		return false, nil
 	}
 
 	cflags := utils.NewStringSlice(l.Properties.Cflags,
 		l.Properties.Export_cflags, l.getExportedCflags(mctx))
 
 	props := &ccLibraryCommonProps{
-		Name:               proptools.StringPtr(l.Name()),
+		Name:               proptools.StringPtr(l.shortName()),
+		Stem:               proptools.StringPtr(l.Name()),
 		Srcs:               utils.Filter(utils.IsCompilableSource, l.Properties.Srcs),
 		Exclude_srcs:       l.Properties.Exclude_srcs,
 		Cflags:             cflags,
 		Include_dirs:       l.Properties.Include_dirs,
 		Local_include_dirs: l.Properties.Local_include_dirs,
-		Static_libs:        l.Properties.ResolvedStaticLibs,
-		Whole_static_libs:  l.Properties.Whole_static_libs,
+		Static_libs:        ccModuleNames(mctx, l.Properties.ResolvedStaticLibs),
+		Whole_static_libs:  ccModuleNames(mctx, l.Properties.Whole_static_libs),
 		Ldflags:            l.Properties.Ldflags,
 	}
 
-	return true, props
+	return props
+}
+
+// Create a module which only builds on the device. The closest thing Soong
+// provides will also allow building on the host, which is not quite what we
+// want.
+func libraryTargetStaticFactory() android.Module {
+	module, library := cc.NewLibrary(android.DeviceSupported)
+	library.BuildOnlyStatic()
+	return module.Init()
 }
 
 func (l *staticLibrary) soongBuildActions(mctx android.TopDownMutatorContext) {
-	enabled, commonProps := l.setupCcLibraryProps(mctx)
-	if !enabled {
+	if !isEnabled(l) {
 		return
 	}
+
+	commonProps := l.setupCcLibraryProps(mctx)
 
 	libProps := &ccStaticOrSharedProps{
 		// Soong's `export_include_dirs` field is relative to the module dir.
 		Export_include_dirs: l.Properties.Export_local_include_dirs,
 	}
 
-	mctx.CreateModule(android.ModuleFactoryAdaptor(cc.LibraryStaticFactory), commonProps, libProps)
+	switch l.Properties.TargetType {
+	case tgtTypeHost:
+		mctx.CreateModule(android.ModuleFactoryAdaptor(cc.LibraryHostStaticFactory), commonProps, libProps)
+	case tgtTypeTarget:
+		mctx.CreateModule(android.ModuleFactoryAdaptor(libraryTargetStaticFactory), commonProps, libProps)
+	}
+}
+
+// From Soong's cc/binary.go. This is needed here because it is not exported by Soong.
+func binaryHostFactory() android.Module {
+	module, _ := cc.NewBinary(android.HostSupported)
+	return module.Init()
+}
+
+// Like libraryTargetStaticFactory, create a module which is only buildable on the device.
+func binaryTargetFactory() android.Module {
+	module, _ := cc.NewBinary(android.DeviceSupported)
+	return module.Init()
 }
 
 func (b *binary) soongBuildActions(mctx android.TopDownMutatorContext) {
-	enabled, commonProps := b.setupCcLibraryProps(mctx)
-	if !enabled {
+	if !isEnabled(b) {
 		return
 	}
 
-	mctx.CreateModule(android.ModuleFactoryAdaptor(cc.BinaryFactory), commonProps)
+	commonProps := b.setupCcLibraryProps(mctx)
+
+	switch b.Properties.TargetType {
+	case tgtTypeHost:
+		mctx.CreateModule(android.ModuleFactoryAdaptor(binaryHostFactory), commonProps)
+	case tgtTypeTarget:
+		mctx.CreateModule(android.ModuleFactoryAdaptor(binaryTargetFactory), commonProps)
+	}
+
 }
