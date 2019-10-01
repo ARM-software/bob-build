5b3ec9030bf9025d65f70d9beb8900ca5288282e
Chris Diamand <chris.diamand@arm.com>
2019-10-01 19:06:27 +0100

    Don't use Soong's genrule for bob_generated_source
    Implement a custom backend module type to handle generated sources,
    libraries, and binaries. This will allow more flexible handling of
    sources vs headers, which Soong's `genrule` does not allow.
    
    Transformed sources will be converted to use the new backend module type
    in a subsequent commit.
    
    For now, the `${xxmod_out}` variable is not implemented, but is
    explicitly set to the empty string. This lets the `generate_source` test
    `build.bp` files parse so that the other tests can run.
    
    Change-Id: I7ee83eebd7d64debc0f827bc910c78ed713f24cd
    Signed-off-by: Chris Diamand <chris.diamand@arm.com>

--- core/soong_gen.go
+++ core/soong_gen.go
@@ -29,8 +29,173 @@ import (
 	"android/soong/android"
 	"android/soong/genrule"
 
+	"github.com/ARM-software/bob-build/utils"
+
+	"github.com/google/blueprint"
 	"github.com/google/blueprint/proptools"
 )
+
+type genBackendProps struct {
+	Srcs                    []string
+	Out                     []string
+	Export_gen_include_dirs []string
+	Cmd                     string
+	HostBin                 string
+	Tool                    string
+	Depfile                 string
+	Module_deps             []string
+	Module_srcs             []string
+	Encapsulates            []string
+}
+
+type genBackend struct {
+	android.ModuleBase
+	Properties genBackendProps
+
+	outputs              android.WritablePaths
+	depfile              android.WritablePath
+	exportGenIncludeDirs android.Paths
+}
+
+func genBackendFactory() android.Module {
+	m := &genBackend{}
+
+	m.AddProperties(&m.Properties)
+	android.InitAndroidModule(m)
+
+	return m
+}
+
+func (m *genBackend) filterOutputs(predicate func(string) bool) (ret android.Paths) {
+	for _, p := range m.outputs {
+		if predicate(p.String()) {
+			ret = append(ret, p)
+		}
+	}
+	return
+}
+
+// GeneratedSourceFiles, GeneratedHeaderDirs and GeneratedDeps implement the
+// genrule.SourceFileGenerator interface, which allows these modules to be used
+// to generate inputs for cc_library and cc_binary modules.
+func (m *genBackend) GeneratedSourceFiles() android.Paths {
+	return m.filterOutputs(utils.IsCompilableSource)
+}
+
+func (m *genBackend) GeneratedHeaderDirs() android.Paths {
+	return m.exportGenIncludeDirs
+}
+
+func (m *genBackend) GeneratedDeps() (srcs android.Paths) {
+	return m.filterOutputs(utils.IsNotCompilableSource)
+}
+
+func (m *genBackend) DepsMutator(mctx android.BottomUpMutatorContext) {
+	if m.Properties.HostBin != "" {
+		mctx.AddFarVariationDependencies([]blueprint.Variation{
+			{Mutator: "arch", Variation: mctx.Config().BuildOsVariant}},
+			hostToolBinTag, m.Properties.HostBin)
+	}
+
+	parseAndAddVariationDeps(mctx, generatedDepTag,
+		m.Properties.Module_deps...)
+	parseAndAddVariationDeps(mctx, generatedSourceTag,
+		m.Properties.Module_srcs...)
+	parseAndAddVariationDeps(mctx, encapsulatesTag,
+		m.Properties.Encapsulates...)
+}
+
+func (m *genBackend) getHostBin(ctx android.ModuleContext) string {
+	if m.Properties.HostBin == "" {
+		return ""
+	}
+	hostBinModule := ctx.GetDirectDepWithTag(m.Properties.HostBin, hostToolBinTag)
+	htp, ok := hostBinModule.(genrule.HostToolProvider)
+	if !ok {
+		panic(fmt.Errorf("%s is not a host tool", m.Properties.HostBin))
+	}
+	return htp.HostToolPath().String()
+}
+
+func (m *genBackend) getArgs(ctx android.ModuleContext) map[string]string {
+	args := map[string]string{
+		"bob_config":      filepath.Join(getBuildDir(), configName),
+		"bob_config_opts": configOpts,
+		"gen_dir":         android.PathForModuleGen(ctx).String(),
+		"host_bin":        m.getHostBin(ctx),
+
+		// flag_defaults is primarily used to invoke sub-makes of
+		// different libraries. This shouldn't be needed on Android.
+		// This means the following can't be expanded:
+		"ar":         "",
+		"as":         "",
+		"asflags":    "",
+		"cc":         "",
+		"cflags":     "",
+		"conlyflags": "",
+		"cxx":        "",
+		"cxxflags":   "",
+		"ldflags":    "",
+		"linker":     "",
+	}
+	// TODO: Support `${xxmod_out}`
+	ctx.VisitDirectDepsIf(
+		func(dep android.Module) bool {
+			tag := ctx.OtherModuleDependencyTag(dep)
+			if tag == generatedSourceTag || tag == generatedDepTag {
+				return true
+			}
+			return false
+		},
+		func(dep android.Module) {
+			args[dep.Name()+"_out"] = ""
+		})
+	return args
+}
+
+func (m *genBackend) GenerateAndroidBuildActions(ctx android.ModuleContext) {
+	args := m.getArgs(ctx)
+	implicits := []android.Path{}
+
+	for _, out := range m.Properties.Out {
+		m.outputs = append(m.outputs, android.PathForModuleGen(ctx, out))
+	}
+
+	for _, dir := range m.Properties.Export_gen_include_dirs {
+		m.exportGenIncludeDirs = append(m.exportGenIncludeDirs, android.PathForModuleGen(ctx, dir))
+	}
+
+	if m.Properties.Tool != "" {
+		tool := android.PathForSource(ctx, filepath.Join(ctx.ModuleDir(), m.Properties.Tool))
+		args["tool"] = tool.String()
+		implicits = append(implicits, tool)
+	}
+
+	if m.Properties.Depfile != "" {
+		m.depfile = android.PathForModuleGen(ctx, m.Properties.Depfile)
+		args["depfile"] = m.depfile.String()
+	}
+
+	rule := ctx.Rule(apctx,
+		"bob_gen_"+ctx.ModuleName(),
+		blueprint.RuleParams{
+			Command: m.Properties.Cmd,
+			Restat:  true,
+		},
+		utils.SortedKeys(args)...,
+	)
+
+	ctx.Build(apctx,
+		android.BuildParams{
+			Rule:        rule,
+			Description: "gen " + ctx.ModuleName(),
+			Inputs:      android.PathsForSource(ctx, m.Properties.Srcs),
+			Implicits:   implicits,
+			Outputs:     m.outputs,
+			Args:        args,
+			Depfile:     m.depfile,
+		})
+}
 
 func expandBobVariables(str, tool, hostBin string) (out string, err error) {
 	// Bob is lax on whether there is any parenthesis, or whether
@@ -111,65 +276,33 @@ func (gc *generateCommon) getHostBinModule(mctx android.TopDownMutatorContext) (
 func (gc *generateCommon) createGenrule(mctx android.TopDownMutatorContext,
 	out []string, depfile string) {
 
-	// Map to Soong's genrule, where a single command produces the
-	// outputs from the inputs.
-	//
-	// Setup a genrule property struct as if blueprint had read it
-	// Only include the fields that we expect to use
-	genProps := struct {
-		Name         *string
-		Srcs         []string
-		Exclude_srcs []string
-		Out          []string
-		Depfile      *bool
-
-		Tool_files []string
-		Tools      []string
-		Cmd        *string
-
-		Export_include_dirs []string
-
-		Owner       *string
-		Proprietary *bool
-
-		Enabled *bool
-	}{}
-
-	genProps.Name = proptools.StringPtr(gc.Name())
-	genProps.Srcs = relativeToModuleDir(mctx, gc.Properties.Srcs)
-	genProps.Exclude_srcs = relativeToModuleDir(mctx, gc.Properties.Exclude_srcs)
-	genProps.Out = out
-	genProps.Export_include_dirs = gc.Properties.Export_gen_include_dirs
-	genProps.Enabled = gc.Properties.Enabled
-
-	// Only set Tool_files or Tool if the Bob property is not ""
-	// otherwise Soong will report a missing dependency
-	if gc.Properties.Tool != "" {
-		genProps.Tool_files = []string{gc.Properties.Tool}
-	}
-	hostBinModuleName := ""
-	if gc.Properties.Host_bin != "" {
-		hostBinModuleName = ccModuleName(mctx, gc.getHostBinModule(mctx).Name())
-		genProps.Tools = []string{hostBinModuleName}
-	}
-
-	// Bob's specified filename will be ignored. Soong will report an
-	// error if $(depfile) is not used in the command
-	genProps.Depfile = proptools.BoolPtr(depfile != "")
-
 	// Replace ${args} immediately
 	cmd := strings.Replace(gc.Properties.Cmd, "${args}",
 		strings.Join(gc.Properties.Args, " "), -1)
 
-	cmd2, err := expandBobVariables(cmd, gc.Properties.Tool, hostBinModuleName)
-	if err != nil {
-		panic(fmt.Errorf("%s property cmd %s", mctx.ModuleName(), err.Error()))
+	hostBinModuleName := ""
+	if gc.Properties.Host_bin != "" {
+		hostBinModuleName = ccModuleName(mctx, gc.getHostBinModule(mctx).Name())
 	}
-	genProps.Cmd = proptools.StringPtr(cmd2)
+
+	nameProps := nameProps{proptools.StringPtr(gc.Name())}
+
+	genProps := genBackendProps{
+		Srcs:                    gc.Properties.getSources(mctx),
+		Out:                     out,
+		Export_gen_include_dirs: gc.Properties.Export_gen_include_dirs,
+		Tool:                    gc.Properties.Tool,
+		HostBin:                 hostBinModuleName,
+		Cmd:                     cmd,
+		Depfile:                 depfile,
+		Module_deps:             gc.Properties.Module_deps,
+		Module_srcs:             gc.Properties.Module_srcs,
+		Encapsulates:            gc.Properties.Encapsulates,
+	}
 
 	// The ModuleDir for the new module will be inherited from the
 	// current module via the TopDownMutatorContext
-	mctx.CreateModule(android.ModuleFactoryAdaptor(genrule.GenRuleFactory), &genProps)
+	mctx.CreateModule(android.ModuleFactoryAdaptor(genBackendFactory), &nameProps, &genProps)
 }
 
 func (gs *generateSource) soongBuildActions(mctx android.TopDownMutatorContext) {
