dafa3628e7ea9b13ffacb41bb6dc1a644e0f336b
Alexander Khabarov <alexander.khabarov@arm.com>
2019-11-08 16:39:11 +0000

    Enable Travis CI tests for macOS
    Update .travis.yml to support macOS.
    
    Change-Id: I01738934c63fe8b02f4bd03432398040fefc82d4
    Signed-off-by: Alexander Khabarov <alexander.khabarov@arm.com>

--- .travis/checks/check-pep8.sh
+++ .travis/checks/check-pep8.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 BOB_ROOT=$(dirname ${0})/../..
-find "${BOB_ROOT}" -name "*.py" -print0 | xargs -0 python -m pep8 --config="${BOB_ROOT}/.pep8"
+find "${BOB_ROOT}" -name "*.py" -print0 \
+    | xargs -0 python${PYTHON_SUFFIX} -m pep8 --config="${BOB_ROOT}/.pep8"
--- .travis/checks/check-pylint.sh
+++ .travis/checks/check-pylint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 BOB_ROOT=$(dirname ${0})/../..
-find "${BOB_ROOT}" -name "*.py" -print0 | xargs -0 python -m pylint --py3k --errors-only
+find "${BOB_ROOT}" -name "*.py" -print0 \
+    | xargs -0 python${PYTHON_SUFFIX} -m pylint --py3k --errors-only
