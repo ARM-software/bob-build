d1d073fe5c66323d4cb8ec85173ee25180d6631f
Alexander Khabarov <alexander.khabarov@arm.com>
2020-02-18 13:01:52 +0000

    Specify cxxflags & conlyflags and refactor ccflags package
    This commit removes redundant exports and calls to `panic` (the errors
    are instead propagated to `bob/core`) from `ccflags` package, and also
    adds code to specify `cxxflags` and `conlyflags` in `core/androidbp_cclibs.go`.
    
    Change-Id: I63187da005aa14d48665c12f8d84b1ec97103407
    Signed-off-by: Alexander Khabarov <alexander.khabarov@arm.com>

--- core/androidbp_test.go
+++ core/androidbp_test.go
@@ -35,48 +35,73 @@ func (m *MockModule) AddString(name, value string) {
 	m.Called(name, value)
 }
 
-func Test_filterCFlags(t *testing.T) {
+func (m *MockModule) AddStringList(name string, values []string) {
+	m.Called(name, values)
+}
+
+func Test_addCFlags(t *testing.T) {
 	m := new(MockModule)
 
 	m.On("AddString", "c_std", "c11")
 	m.On("AddString", "cpp_std", "c++11")
 	m.On("AddString", "instruction_set", "arm")
+	m.On("AddStringList", "cflags", []string{"-cl-no-signed-zeros"})
+	m.On("AddStringList", "conlyflags", []string(nil))
+	m.On("AddStringList", "cxxflags", []string(nil))
 
 	cflags := []string{"-marm", "-mx32", "-cl-no-signed-zeros"}
 	conlyFlags := []string{"-std=c11"}
 	cxxFlags := []string{"-std=c++11"}
 
-	assert.Equal(t, []string{"-cl-no-signed-zeros"}, filterCFlags(m, cflags, conlyFlags, cxxFlags))
+	addCFlags(m, cflags, conlyFlags, cxxFlags)
 
 	m.AssertExpectations(t)
 }
 
-func Test_filterCFlags2(t *testing.T) {
+func Test_addCFlags2(t *testing.T) {
 	m := new(MockModule)
 
 	m.On("AddString", "c_std", "c17")
 	m.On("AddString", "cpp_std", "c++17")
 	m.On("AddString", "instruction_set", "thumb")
+	m.On("AddStringList", "cflags", []string(nil))
+	m.On("AddStringList", "conlyflags", []string(nil))
+	m.On("AddStringList", "cxxflags", []string(nil))
 
 	cflags := []string{"-mthumb", "-mx32"}
 	conlyFlags := []string{"-std=c17"}
 	cxxFlags := []string{"-std=c++17"}
 
-	assert.Equal(t, []string(nil), filterCFlags(m, cflags, conlyFlags, cxxFlags))
+	addCFlags(m, cflags, conlyFlags, cxxFlags)
 
 	m.AssertExpectations(t)
 }
 
-func Test_filterCFlags3(t *testing.T) {
+func Test_addCFlags3(t *testing.T) {
 	m := new(MockModule)
 
 	m.On("AddString", "cpp_std", "c++17")
+	m.On("AddStringList", "cflags", []string(nil))
+	m.On("AddStringList", "conlyflags", []string(nil))
+	m.On("AddStringList", "cxxflags", []string(nil))
 
 	cflags := []string{"-mx32"}
 	conlyFlags := []string{}
 	cxxFlags := []string{"-std=c++17"}
 
-	assert.Equal(t, []string(nil), filterCFlags(m, cflags, conlyFlags, cxxFlags))
+	addCFlags(m, cflags, conlyFlags, cxxFlags)
 
 	m.AssertExpectations(t)
+}
+
+func Test_addCFlags4(t *testing.T) {
+	m := new(MockModule)
+
+	cflags := []string{"-marm", "-mthumb"}
+	conlyFlags := []string{}
+	cxxFlags := []string{}
+
+	err := addCFlags(m, cflags, conlyFlags, cxxFlags)
+
+	assert.Equal(t, err.Error(), "Both thumb and no thumb (arm) options are specified")
 }
