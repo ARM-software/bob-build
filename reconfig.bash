6e429d211000869c0eb8127363cc4cd8e9abb497
Sebastian Birunt <sebastian.birunt@arm.com>
2020-08-10 17:50:35 +0200

    Reconfigure and print configuration for build directory
    Switching between build directories does not allow easily
    to verify what user configuration has been previously set.
    
    Add script which allows reconfiguration based on previous
    configuration set.
    Add script which prints all options which has been set by the user.
    
    Change-Id: I7f856af80453e1b6304aae262ecdd51f35256a43
    Signed-off-by: Sebastian Birunt <sebastian.birunt@arm.com>

---
+++ reconfig.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Copyright 2020 Arm Limited.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+trap 'echo "*** Unexpected error ***"' ERR
+
+ORIG_PWD="$(pwd)"
+
+# Move to the build directory
+cd $(dirname "${BASH_SOURCE[0]}")
+
+source ".bob.bootstrap"
+
+# Move to the working directory
+cd -P "${WORKDIR}"
+
+"${BOB_DIR}/config_system/update_config.py" -d "${SRCDIR}/Mconfig" \
+    ${BOB_CONFIG_OPTS} ${BOB_CONFIG_PLUGIN_OPTS} \
+    -j "${CONFIG_JSON}" \
+    -c "${CONFIG_FILE}" \
+    --depfile "${CONFIG_FILE}.d"
