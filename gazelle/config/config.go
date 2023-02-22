1eab9c26735d76ccd667a863d0a1c3a46eb165cd
Lukasz Okraszewski <lukasz.okraszewski@arm.com>
2023-02-22 16:08:31 +0000

    feat(gazelle): add config structure
    `Configure` runs in every directory of the project,
    the plugin configuration is propagated down the directory
    tree by inheriting the values from its parent.
    
    The `bob_root` directive is used to mark the root module
    of the Bob workspace such that the plugin will parse
    the files in that tree recursively.
    
    This allows the plugin to support multiple Bob workspaces
    in a monorepo setup.
    
    Change-Id: I310083b582bfbca0d9d1754486769502d4cf6d35

---
+++ gazelle/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"path/filepath"
+)
+
+// Config stores the plugin configuration, it is created for each directory in which the plugin runs.
+// The structure is designed to inherit parent configuration to propagate settings on a tree level.
+type Config struct {
+	parent                  *Config
+	RepositoryRootPath      string // Absolute path to the root of the workspace.
+	BobWorkspaceRootRelPath string // Relative module path to Bob workspace root.
+}
+
+func (c *Config) NewChild() *Config {
+	return &Config{
+		parent:                  c,
+		RepositoryRootPath:      c.RepositoryRootPath,
+		BobWorkspaceRootRelPath: c.BobWorkspaceRootRelPath,
+	}
+}
+
+func NewRootConfig(repositoryRootPath string) *Config {
+	return &Config{
+		parent:                  nil,
+		RepositoryRootPath:      repositoryRootPath,
+		BobWorkspaceRootRelPath: "",
+	}
+}
+
+// Map of configs keyed on relative module paths.
+type ConfigMap map[string]*Config
+
+func (c *ConfigMap) ParentForModulePath(relativeModulePath string) *Config {
+	dir := filepath.Dir(relativeModulePath)
+	if dir == "." {
+		dir = ""
+	}
+	parent := (map[string]*Config)(*c)[dir]
+	return parent
+}
