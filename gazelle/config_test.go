d0ce5143d1260949ac3062682e4b487d3b45210b
Sebastian Birunt <sebastian.birunt@arm.com>
2023-03-13 08:33:38 +0100

    test: add tests for config.go
    Add tests for `createBobConfigSpoof`
    
    Change-Id: I2c73afb3a481138e746ca4c494c062c5e8cf8e44
    Signed-off-by: Sebastian Birunt <sebastian.birunt@arm.com>

---
+++ gazelle/config_test.go
@@ -0,0 +1,35 @@
+package plugin
+
+import (
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"testing"
+)
+
+func TestBobConfigSpoof(t *testing.T) {
+
+	var configs map[string]configData = make(map[string]configData)
+
+	configs["feature_x"] = configData{Type: "config", Datatype: "bool"}
+	configs["feature_y"] = configData{Type: "config", Datatype: "string"}
+
+	bobConfig := createBobConfigSpoof(&configs)
+
+	assert.Equal(t, len(bobConfig.Properties.FeatureList), 2, "Wrong features inside config")
+	assert.Contains(t, bobConfig.Properties.FeatureList, "feature_x")
+	assert.Contains(t, bobConfig.Properties.FeatureList, "feature_y")
+
+	if v, ok := bobConfig.Properties.Properties["feature_x"].(configData); ok {
+		assert.Equal(t, "bool", v.Datatype)
+		assert.Equal(t, "config", v.Type)
+	} else {
+		t.Errorf("configData.Datatype of 'feature_x' has wrong type: %s", reflect.TypeOf(v))
+	}
+
+	if v, ok := bobConfig.Properties.Properties["feature_y"].(configData); ok {
+		assert.Equal(t, "string", v.Datatype)
+		assert.Equal(t, "config", v.Type)
+	} else {
+		t.Errorf("configData.Datatype of 'feature_y' has wrong type: %s", reflect.TypeOf(v))
+	}
+}
