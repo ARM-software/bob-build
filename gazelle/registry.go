6d8cb476f9dbc57d34b13a6da8b724dc15d55f14
Sebastian Birunt <sebastian.birunt@arm.com>
2023-03-21 07:29:16 +0100

    refactor: do not use pointer to interfaces
    There is no need to use pointer to interfaces, which can be
    compared to double pointer to object.
    
    Change-Id: I69031a2b9ef8dfedda92943f49030a9c1ad900de
    Signed-off-by: Sebastian Birunt <sebastian.birunt@arm.com>

--- gazelle/registry.go
+++ gazelle/registry.go
@@ -9,41 +9,46 @@ type Registrable interface {
 }
 
 type Registry struct {
-	globalNameMap map[string]*Registrable
-	labelMap      map[label.Label]*Registrable
-	pathMap       map[string][]*Registrable
+	globalNameMap map[string]Registrable
+	labelMap      map[label.Label]Registrable
+	pathMap       map[string][]Registrable
 }
 
 func (r *Registry) register(m Registrable) {
-	r.globalNameMap[m.getName()] = &m
-	r.pathMap[m.getRelativePath()] = append(r.pathMap[m.getRelativePath()], &m)
-	r.labelMap[m.getLabel()] = &m
+	r.globalNameMap[m.getName()] = m
+	r.pathMap[m.getRelativePath()] = append(r.pathMap[m.getRelativePath()], m)
+	r.labelMap[m.getLabel()] = m
 }
 
 func (r *Registry) nameExists(name string) bool {
-	return r.globalNameMap[name] != nil
+	_, ok := r.globalNameMap[name]
+	return ok
 }
 
 func (r *Registry) labelExists(l label.Label) bool {
-	return r.labelMap[l] != nil
+	_, ok := r.labelMap[l]
+	return ok
 }
 
-func (r *Registry) retrieveByName(name string) *Registrable {
-	return r.globalNameMap[name]
+func (r *Registry) retrieveByName(name string) (reg Registrable, ok bool) {
+	reg, ok = r.globalNameMap[name]
+	return reg, ok
 }
 
-func (r *Registry) retrieveByLabel(l label.Label) *Registrable {
-	return r.labelMap[l]
+func (r *Registry) retrieveByLabel(l label.Label) (reg Registrable, ok bool) {
+	reg, ok = r.labelMap[l]
+	return reg, ok
 }
 
-func (r *Registry) retrieveByPath(path string) []*Registrable {
-	return r.pathMap[path]
+func (r *Registry) retrieveByPath(path string) (regs []Registrable, ok bool) {
+	regs, ok = r.pathMap[path]
+	return regs, ok
 }
 
 func NewRegistry() *Registry {
 	return &Registry{
-		globalNameMap: map[string]*Registrable{},
-		labelMap:      map[label.Label]*Registrable{},
-		pathMap:       map[string][]*Registrable{},
+		globalNameMap: map[string]Registrable{},
+		labelMap:      map[label.Label]Registrable{},
+		pathMap:       map[string][]Registrable{},
 	}
 }
--- gazelle/registry_test.go
+++ gazelle/registry_test.go
@@ -9,14 +9,19 @@ func Test_register_module(t *testing.T) {
 	registry := NewRegistry()
 	testLabel := label.Label{Repo: "repo", Pkg: "some/pkg", Name: "m_name"}
 	m := NewModule("m_name", "bob_binary", "some/pkg", "repo")
+
 	registry.register(m)
+
 	if !registry.nameExists("m_name") {
-		t.Errorf("module %d not successfully registered", m.getName())
+		t.Errorf("module %s not successfully registered", m.getName())
 	}
+
 	if !registry.labelExists(testLabel) {
-		t.Errorf("module %d not successfully registered", m.getName())
+		t.Errorf("module %s not successfully registered", m.getName())
 	}
-	if registry.retrieveByPath("some/pkg") == nil {
-		t.Errorf("module %d not successfully registered", m.getName())
+
+	if _, ok := registry.retrieveByPath("some/pkg"); !ok {
+		t.Errorf("module %s not successfully registered", m.getName())
 	}
+
 }
