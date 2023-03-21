572fd10cfb57063d9fdee49b9b20fb65841fb53e
Sebastian Birunt <sebastian.birunt@arm.com>
2023-03-21 09:21:56 +0100

    chore: add moduletypes & `bobKinds`
    Modules types will help differentiate particular
    types while generating rules.
    
    Change-Id: I4a1c10dc33c3d2d684670805e0e6bbf964eb2b74
    Signed-off-by: Sebastian Birunt <sebastian.birunt@arm.com>

--- gazelle/module.go
+++ gazelle/module.go
@@ -1,20 +1,67 @@
 package plugin
 
 import (
+	"log"
+	"strings"
+
 	"github.com/bazelbuild/bazel-gazelle/label"
 )
 
+type ModuleType int8
+
+const (
+	ModuleUndefined ModuleType = iota
+	ModuleBinary
+	ModuleLibrary
+	ModuleFilegroup
+	ModuleGlob
+)
+
+var (
+	bobToModuleTypeMap = map[string]ModuleType{
+		"bob_filegroup":       ModuleFilegroup,
+		"bob_glob":            ModuleGlob,
+		"bob_binary":          ModuleBinary,
+		"bob_static_library":  ModuleLibrary,
+		"bob_dynamic_library": ModuleLibrary,
+		"bob_library":         ModuleLibrary,
+	}
+)
+
+func (t ModuleType) String() string {
+	switch t {
+	case ModuleBinary:
+		return "cc_binary"
+	case ModuleLibrary:
+		return "cc_library"
+	case ModuleFilegroup:
+		return "filegroup"
+	}
+	return "unknown"
+}
+
+func ParseModuleType(str string) ModuleType {
+	if t, ok := bobToModuleTypeMap[strings.ToLower(str)]; ok {
+		return t
+	} else {
+		log.Printf("Undefined module type: %s\n", str)
+		return ModuleUndefined
+	}
+}
+
 type AttributesMap map[string]interface{}
+
 type Module struct {
-	bobName      string
+	name         string
 	bobType      string
+	moduleType   ModuleType
 	relativePath string
 	bazelLabel   label.Label
 	features     map[string]AttributesMap
 }
 
 func (m Module) getName() string {
-	return m.bobName
+	return m.name
 }
 
 func (m Module) getRelativePath() string {
@@ -38,8 +85,9 @@ func (m *Module) addFeatureAttribute(feature string, attribute string, v interfa
 func NewModule(moduleName string, moduleType string, relPath string, rootPath string) *Module {
 
 	m := &Module{}
-	m.bobName = moduleName
+	m.name = moduleName
 	m.bobType = moduleType
+	m.moduleType = ParseModuleType(moduleType)
 	m.relativePath = relPath
 	m.bazelLabel = label.Label{Repo: rootPath, Pkg: relPath, Name: moduleName}
 	m.features = make(map[string]AttributesMap)
