18f97658396f36435865c87edd10ee438375a44f
Lukasz Okraszewski <lukasz.okraszewski@arm.com>
2023-06-14 12:49:58 +0100

    fix(toolchain): fixed empty flag string
    Prior to this fix the GNU flags were not checked
    for empty string, resulting in a empty string
    being appended to cflags and ldflags.
    
    Change-Id: Id6b48bac2d2682162322e279b8e2c639faca8796

--- core/toolchain/gnu.go
+++ core/toolchain/gnu.go
@@ -199,9 +199,12 @@ func newToolchainGnuCommon(props *config.Properties, tgt TgtType) (tc toolchainG
 		tc.ldflags = append(tc.ldflags, "--sysroot="+sysroot)
 	}
 
-	flags := strings.Split(props.GetString(string(tgt)+"_gnu_flags"), " ")
-	tc.cflags = append(tc.cflags, flags...)
-	tc.ldflags = append(tc.ldflags, flags...)
+	gnuFlagsProp := props.GetString(string(tgt) + "_gnu_flags")
+	if gnuFlagsProp != "" {
+		flags := strings.Split(gnuFlagsProp, " ")
+		tc.cflags = append(tc.cflags, flags...)
+		tc.ldflags = append(tc.ldflags, flags...)
+	}
 
 	tc.linker = newDefaultLinker(tc.gxxBinary, tc.ldflags, []string{})
 	tc.flagCache = newFlagCache()
