2cd8565773a39aef89daf56a5ef2631fead20f4d
Sebastian Birunt <sebastian.birunt@arm.com>
2023-07-04 09:49:45 +0200

    chore: separate building rules
    Building rules part can be separated
    and reused also for strict generation
    modules after resolving all arguments.
    
    Signed-off-by: Sebastian Birunt <sebastian.birunt@arm.com>
    Change-Id: I32af57e06e50082afe1770c337e9f2daf8b2e019

--- core/linux_generated.go
+++ core/linux_generated.go
@@ -42,6 +42,66 @@ var touchRule = pctx.StaticRule("touch",
 		Description: "touch $out",
 	})
 
+type ruleContext struct {
+	rule      *blueprint.Rule
+	inouts    []inout
+	args      map[string]string
+	implicits []string
+}
+
+func (g *linuxGenerator) buildRules(r *ruleContext, ctx blueprint.ModuleContext) {
+
+	for _, inout := range r.inouts {
+		r.args["_out_"] = utils.Join(inout.out)
+		if inout.rspfile != "" {
+			r.args["rspfile"] = inout.rspfile
+		}
+
+		mainRuleOuts := inout.out
+		mainRuleImplicitOuts := inout.implicitOuts
+		deps := blueprint.DepsNone
+
+		// ninja currently does not support the case when depfile is
+		// defined and multiple outputs at the same time. So adjust the
+		// main rule to have a single output, and link the remaining
+		// outputs using a separate rule.
+		if inout.depfile != "" && (len(inout.out)+len(inout.implicitOuts)) > 1 {
+			// No-op rule linking the extra outputs to the main
+			// output. Update the extra outputs' mtime in case the
+			// script actually creates the extra outputs first.
+
+			allOutputs := append(mainRuleOuts, mainRuleImplicitOuts...)
+			mainRuleOuts = allOutputs[0:1]
+			mainRuleImplicitOuts = []string{}
+
+			ctx.Build(pctx,
+				blueprint.BuildParams{
+					Rule:     touchRule,
+					Inputs:   mainRuleOuts,
+					Outputs:  allOutputs[1:],
+					Optional: true,
+				})
+			deps = blueprint.DepsGCC
+		}
+
+		unique_implicits := utils.Unique(append(inout.implicitSrcs, r.implicits...))
+
+		buildparams := blueprint.BuildParams{
+			Rule:            *r.rule,
+			Inputs:          inout.in,
+			Outputs:         mainRuleOuts,
+			ImplicitOutputs: mainRuleImplicitOuts,
+			Implicits:       unique_implicits,
+			Args:            r.args,
+			Optional:        true,
+			Depfile:         inout.depfile,
+			Deps:            deps,
+		}
+
+		ctx.Build(pctx, buildparams)
+	}
+}
+
 // Generate the build actions for a generateSource module and populates the outputs.
 func (g *linuxGenerator) generateCommonActions(m *ModuleGenerateCommon, ctx blueprint.ModuleContext, inouts []inout) {
 	outputdir := backend.Get().SourceOutputDir(ctx.Module())
@@ -79,55 +139,14 @@ func (g *linuxGenerator) generateCommonActions(m *ModuleGenerateCommon, ctx blue
 	rule := ctx.Rule(pctx, "gen_"+m.Name(), ruleparams,
 		append(utils.SortedKeys(args), "depfile", "rspfile", "_out_")...)
 
-	for _, inout := range inouts {
-		args["_out_"] = utils.Join(inout.out)
-		if inout.rspfile != "" {
-			args["rspfile"] = inout.rspfile
-		}
-
-		mainRuleOuts := inout.out
-		mainRuleImplicitOuts := inout.implicitOuts
-		deps := blueprint.DepsNone
-
-		// ninja currently does not support the case when depfile is
-		// defined and multiple outputs at the same time. So adjust the
-		// main rule to have a single output, and link the remaining
-		// outputs using a separate rule.
-		if inout.depfile != "" && (len(inout.out)+len(inout.implicitOuts)) > 1 {
-			// No-op rule linking the extra outputs to the main
-			// output. Update the extra outputs' mtime in case the
-			// script actually creates the extra outputs first.
-
-			allOutputs := append(mainRuleOuts, mainRuleImplicitOuts...)
-			mainRuleOuts = allOutputs[0:1]
-			mainRuleImplicitOuts = []string{}
-
-			ctx.Build(pctx,
-				blueprint.BuildParams{
-					Rule:     touchRule,
-					Inputs:   mainRuleOuts,
-					Outputs:  allOutputs[1:],
-					Optional: true,
-				})
-			deps = blueprint.DepsGCC
-		}
-
-		unique_implicits := utils.Unique(append(inout.implicitSrcs, implicits...))
-
-		buildparams := blueprint.BuildParams{
-			Rule:            rule,
-			Inputs:          inout.in,
-			Outputs:         mainRuleOuts,
-			ImplicitOutputs: mainRuleImplicitOuts,
-			Implicits:       unique_implicits,
-			Args:            args,
-			Optional:        true,
-			Depfile:         inout.depfile,
-			Deps:            deps,
-		}
-
-		ctx.Build(pctx, buildparams)
+	rCtx := ruleContext{
+		rule:      &rule,
+		inouts:    inouts,
+		args:      args,
+		implicits: implicits,
 	}
+
+	g.buildRules(&rCtx, ctx)
 }
 
 func transformCmdAndroidToOld(cmd string, agr *ModuleGenruleCommon) (retCmd *string) {
