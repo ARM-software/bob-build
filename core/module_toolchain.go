72263e99508614d22a9573244b1ab39c06e85dac
Sebastian Birunt <sebastian.birunt@arm.com>
2023-08-08 12:04:28 +0200

    chore: add `ModuleToolchain` properties
    Fill `ModuleToolchain` with properties.
    
    Signed-off-by: Sebastian Birunt <sebastian.birunt@arm.com>
    Change-Id: I3ce3eed059b67357ad8fbbaac9790995ebe60f9f

--- core/module_toolchain.go
+++ core/module_toolchain.go
@@ -4,10 +4,38 @@ import (
 	"github.com/ARM-software/bob-build/core/module"
 )
 
+type ModuleToolchainProps struct {
+	// Flags that will be used for C and C++ compiles.
+	Cflags []string
+
+	// Flags that will be used for C compiles.
+	Conlyflags []string
+
+	// Flags that will be used for C++ compiles.
+	Cppflags []string
+
+	// Flags that will be used for .S compiles.
+	Asflags []string
+
+	// Flags that will be used for all link steps.
+	Ldflags []string
+}
+
 // Strict targets will not support defaults by design.
 //
 // With this in mind, we will need a way to propagate
 // common toolchain flags to targets (optimization etc).
 type ModuleToolchain struct {
 	module.ModuleBase
+
+	SplittableProps
+
+	Properties struct {
+		ModuleToolchainProps
+
+		Target TargetSpecific
+		Host   TargetSpecific
+
+		Features
+	}
 }
