6f3c45969e8815711bafbe17616503232570c6a9
Sebastian Birunt <sebastian.birunt@arm.com>
2023-08-09 08:15:18 +0200

    feat: new `bob_toolchain` module
    New `bob_toolchain` rule can be instantiated
    within `build.bp` files which will provide
    toolchain flags for strict modules.
    
    `bob_library` & `bob_executable` can specify
    flgs they want to consume by `toolchains`
    property, however they are not _delivered_
    to final module yet.
    
    Signed-off-by: Sebastian Birunt <sebastian.birunt@arm.com>
    Change-Id: I55b5d2863bb41615eace8962f8e7d79424ded95b

---
+++ tests/toolchain_flags/f_complex.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "f_complex.h"
+
+void f_complex() {
+    std::cout << "f_complex" << std::endl;
+}
---
+++ tests/toolchain_flags/f_simple.h
@@ -0,0 +1,14 @@
+#ifndef _F_SIMPLE_H_
+#define _F_SIMPLE_H_
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+void f_simple();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _F_SIMPLE_H_
