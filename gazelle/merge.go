41053402f8b116c7429f53f710a398b5ae683eb4
Sebastian Birunt <sebastian.birunt@arm.com>
2023-08-21 20:23:35 +0200

    chore: custom mergable struct
    Add new structure type which implements
    `rule.Merger` interface for custom merging
    of rule attributes.
    
    This will allow to avoid Gazelle merging
    of `select()` calls which are sticked to
    Gazelle's defined platforms. Reference to:
    https://github.com/bazelbuild/bazel-gazelle/issues/1051
    
    Signed-off-by: Sebastian Birunt <sebastian.birunt@arm.com>
    Change-Id: I0026d09aa68d513a8c92855fdfe6e58cb30b031c

---
+++ gazelle/merge.go
@@ -0,0 +1,33 @@
+package plugin
+
+import (
+	"github.com/bazelbuild/bazel-gazelle/rule"
+	bzl "github.com/bazelbuild/buildtools/build"
+)
+
+// SrcsAttribute represents `srcs` attribute which
+// can contain complex expression with `glob` & `select`.
+//
+// It implements `rule.Merger` interface thus can be used
+// with custom merge algorithm.
+//
+// Main purpose is to avoid Gazelle drawback where merging
+// `select` is sticked with defined platforms only:
+// https://github.com/bazelbuild/bazel-gazelle/issues/1051
+type SrcsAttribute struct {
+	expr bzl.Expr
+}
+
+var _ rule.BzlExprValue = (*SrcsAttribute)(nil)
+var _ rule.Merger = (*SrcsAttribute)(nil)
+
+func (m *SrcsAttribute) Merge(other bzl.Expr) bzl.Expr {
+	// TODO: implement custom merging
+	// For now return newly generated expression.
+	// It means it will replace existing expression.
+	return m.expr
+}
+
+func (m SrcsAttribute) BzlExpr() bzl.Expr {
+	return m.expr
+}
