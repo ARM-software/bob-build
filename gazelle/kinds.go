f7237efdf757f50a56ccb8688b97275ecd4b742b
Sebastian Birunt <sebastian.birunt@arm.com>
2023-08-21 22:14:27 +0200

    chore: set KindInfo
    Set proper `rule.KindInfo` map for generated
    rules.
    Add test for merging rule's attributes.
    
    Signed-off-by: Sebastian Birunt <sebastian.birunt@arm.com>
    Change-Id: I5d25ca09f9b065c206e1202de9149f628102b7ff

--- gazelle/kinds.go
+++ gazelle/kinds.go
@@ -7,16 +7,63 @@ import (
 var bobKinds = map[string]rule.KindInfo{
 	"filegroup": {
 		NonEmptyAttrs:  map[string]bool{"srcs": true},
-		MergeableAttrs: map[string]bool{"srcs": false},
+		MergeableAttrs: map[string]bool{"srcs": true},
 	},
 	"bool_flag": {
-		NonEmptyAttrs: map[string]bool{"name": true},
+		NonEmptyAttrs: map[string]bool{
+			"name":                  true,
+			"build_setting_default": true,
+		},
+		MergeableAttrs: map[string]bool{"build_setting_default": true},
 	},
 	"string_flag": {
-		NonEmptyAttrs: map[string]bool{"name": true},
+		NonEmptyAttrs: map[string]bool{
+			"name":                  true,
+			"build_setting_default": true,
+		},
+		MergeableAttrs: map[string]bool{"build_setting_default": true},
+	},
+	"int_flag": {
+		NonEmptyAttrs: map[string]bool{
+			"name":                  true,
+			"build_setting_default": true,
+		},
+		MergeableAttrs: map[string]bool{"build_setting_default": true},
+	},
+	"config_setting": {
+		NonEmptyAttrs:  map[string]bool{"name": true},
+		MergeableAttrs: map[string]bool{"flag_values": false},
 	},
 	"selects.config_setting_group": {
 		NonEmptyAttrs: map[string]bool{"name": true},
+		MergeableAttrs: map[string]bool{
+			"match_all": true,
+			"match_any": true,
+		},
+	},
+	"cc_library": {
+		NonEmptyAttrs: map[string]bool{
+			"srcs": true,
+			"hdrs": true,
+			"deps": true,
+		},
+		MergeableAttrs: map[string]bool{
+			"srcs":          true,
+			"hdrs":          true,
+			"local_defines": true,
+			"defines":       true,
+			"copts":         true,
+			"alwayslink":    true,
+			"linkstatic":    true,
+		},
+		SubstituteAttrs: map[string]bool{
+			"srcs": true,
+			"hdrs": true,
+			"deps": true,
+		},
+		ResolveAttrs: map[string]bool{
+			"deps": true,
+		},
 	},
 }
 
