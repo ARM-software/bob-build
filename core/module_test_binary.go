766e2d98a23774f4b8486a261388186a8eee941c
Sebastian Birunt <sebastian.birunt@arm.com>
2023-09-05 13:47:32 +0200

    feat: new `bob_test` module
    Add definition for `bob_test` module.
    Introduced to be transitioned into
    `cc_test` Bazel's module.
    
    Signed-off-by: Sebastian Birunt <sebastian.birunt@arm.com>
    Change-Id: I561c22f2028bb1b32b999e8e6d5ad80fc644a1a8

---
+++ core/module_test_binary.go
@@ -0,0 +1,23 @@
+package core
+
+import "github.com/google/blueprint"
+
+type ModuleTest struct {
+	ModuleStrictBinary
+}
+
+func (m *ModuleTest) GenerateBuildActions(ctx blueprint.ModuleContext) {
+	// TODO: implement
+}
+
+func executableTestFactory(config *BobConfig) (blueprint.Module, []interface{}) {
+	t := true
+
+	module := &ModuleTest{}
+	module.Properties.Linkstatic = &t // always true for executables
+	module.Properties.Features.Init(&config.Properties, StrictLibraryProps{}, SplittableProps{}, InstallableProps{}, EnableableProps{}, IncludeProps{})
+	module.Properties.Host.init(&config.Properties, StrictLibraryProps{}, InstallableProps{}, IncludeProps{})
+	module.Properties.Target.init(&config.Properties, StrictLibraryProps{}, InstallableProps{}, IncludeProps{})
+	return module, []interface{}{&module.Properties,
+		&module.SimpleName.Properties}
+}
