3fcd40151fa8c7418c843a54b1bb4d3a747037c4
Lukasz Okraszewski <lukasz.okraszewski@arm.com>
2023-11-15 08:01:00 +0000

    refactor(gazelle): add a new mconfig parser
    Marks the old parser as Legacy. The new parser
    handles a single directory at a time and is capable
    of reserving labels in the mapper.
    
    Change-Id: I71170861359fdcb7fd6b9d664c988fcddec67a37

--- gazelle/mconfig/parser/parser.go
+++ gazelle/mconfig/parser/parser.go
@@ -15,6 +15,8 @@ import (
 	"time"
 
 	bob "github.com/ARM-software/bob-build/core"
+	"github.com/ARM-software/bob-build/gazelle/logic"
+	"github.com/ARM-software/bob-build/gazelle/mapper"
 	"github.com/ARM-software/bob-build/gazelle/registry"
 	"github.com/bazelbuild/bazel-gazelle/label"
 	"github.com/bazelbuild/rules_go/go/tools/bazel"
@@ -69,26 +71,38 @@ func init() {
 	}()
 }
 
-// MconfigParser implements a parser for Mconfig files that extracts configs
-type MconfigParser struct {
+// Parser implements a parser for Mconfig files that extracts configs
+type Parser struct {
 	// The value of `language.GenerateArgs.Config.RepoRoot`.
 	repoRoot string
 
 	// The value of `language.GenerateArgs.Rel`.
 	relPackagePath string
+
+	m *mapper.Mapper
+}
+
+type Expression struct {
+	Key        string
+	Expression []interface{}
+}
+
+type ConditionalDefault struct {
+	Condition  interface{}   `json:"cond"`
+	Expression []interface{} `json:"expr"`
 }
 
 type ConfigData struct {
-	Datatype   string      `json:"datatype"`
-	RelPath    string      `json:"relPath"`
-	Type       string      `json:"type"`
-	Default    interface{} `json:"default"`
-	Condition  interface{} `json:"default_cond"`
-	Ignore     string      `json:"bob_ignore,omitempty"`
-	Depends    interface{} `json:"depends"`
-	Position   uint32      `json:"position"`
-	BazelLabel label.Label
-	Name       string
+	Datatype            string               `json:"datatype"`
+	RelPath             string               `json:"relPath"`
+	Type                string               `json:"type"`
+	Default             []interface{}        `json:"default"`
+	ConditionalDefaults []ConditionalDefault `json:"default_cond"`
+	Ignore              string               `json:"bob_ignore,omitempty"`
+	Depends             interface{}          `json:"depends"`
+	Position            uint32               `json:"position"`
+	BazelLabel          label.Label          //TODO: remove me
+	Name                string
 }
 
 var _ registry.Registrable = (*ConfigData)(nil)
@@ -105,15 +119,77 @@ func (c *ConfigData) GetLabel() label.Label {
 	return c.BazelLabel
 }
 
-// Constructs a new `MconfigParser`
-func NewMconfigParser(repoRoot string, relPackagePath string) *MconfigParser {
-	return &MconfigParser{
+func New(m *mapper.Mapper) *Parser {
+	return &Parser{
+		m: m,
+	}
+}
+
+func (p *Parser) Parse(
+	rootPath, pkgPath, fileName string) (*map[string]*ConfigData, error) {
+
+	parserMutex.Lock()
+	defer parserMutex.Unlock()
+
+	var configs map[string]*ConfigData
+
+	req := map[string]interface{}{
+		"root_path":        rootPath,
+		"rel_package_path": pkgPath,
+		"file_name":        fileName,
+		"ignore_source":    true,
+	}
+
+	encoder := json.NewEncoder(parserStdin)
+	if err := encoder.Encode(&req); err != nil {
+		return nil, fmt.Errorf("failed to encode: %w", err)
+	}
+
+	reader := bufio.NewReader(parserStdout)
+	data, err := reader.ReadBytes(0)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read: %w", err)
+	}
+
+	if len(data) > 0 {
+		// remove delimiter
+		data = data[:len(data)-1]
+		// fmt.Printf("%s\n", data)
+		err = json.Unmarshal(data, &configs)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal: %w", err)
+		}
+
+		for target := range configs {
+			configs[target].Name = target
+			label := mapper.MakeLabel(target, pkgPath)
+			p.m.Map(label, target)
+
+			truthyExpr := &logic.Identifier{target}
+			falsyExpr := &logic.Not{&logic.Identifier{target}}
+
+			enabled := mapper.MakeLabel(truthyExpr.String(), pkgPath)
+			disabled := mapper.MakeLabel(falsyExpr.String(), pkgPath)
+
+			// Reserve enabled/disabled labels for future generation. This is important as any Mconfig can
+			// refer to any other Mconfig
+			p.m.Map(enabled, truthyExpr)
+			p.m.Map(disabled, falsyExpr)
+		}
+	}
+
+	return &configs, nil
+}
+
+// Constructs a new `Parser`
+func NewLegacy(repoRoot string, relPackagePath string) *Parser {
+	return &Parser{
 		repoRoot:       repoRoot,
 		relPackagePath: relPackagePath,
 	}
 }
 
-func (p *MconfigParser) Parse(fileNames *[]string) (*map[string]*ConfigData, error) {
+func (p *Parser) ParseLegacy(fileNames *[]string) (*map[string]*ConfigData, error) {
 	parserMutex.Lock()
 	defer parserMutex.Unlock()
 
