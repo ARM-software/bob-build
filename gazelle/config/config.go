2575480db2ca6cd309b37679b76d2da705d312bb
Lukasz Okraszewski <lukasz.okraszewski@arm.com>
2023-11-15 08:07:02 +0000

    refactor(gazelle): construct the config
    This configuration structure will be required
    once the generator is switched over to dir based.
    
    Change-Id: Ie01de89bb68d1807c1482c8adf2ed2c798555ec2

--- gazelle/config/config.go
+++ gazelle/config/config.go
@@ -2,7 +2,28 @@ package config
 
 import (
 	"path/filepath"
+
+	bb "github.com/ARM-software/bob-build/gazelle/blueprint/builder"
+	bp "github.com/ARM-software/bob-build/gazelle/blueprint/parser"
+	lb "github.com/ARM-software/bob-build/gazelle/logic/builder"
+	"github.com/ARM-software/bob-build/gazelle/mapper"
+	mb "github.com/ARM-software/bob-build/gazelle/mconfig/builder"
+	mp "github.com/ARM-software/bob-build/gazelle/mconfig/parser"
 )
+
+type MconfigConfig struct {
+	Parser    *mp.Parser
+	Builder   *mb.Builder
+	Filenames []string // Filenames to parse within a directory
+}
+type BlueprintConfig struct {
+	Parser    *bp.Parser
+	Builder   *bb.Builder
+	Filenames []string // Filenames to parse within a directory
+}
+type LogicConfig struct {
+	Builder *lb.Builder
+}
 
 // Config stores the plugin configuration, it is created for each directory in which the plugin runs.
 // The structure is designed to inherit parent configuration to propagate settings on a tree level.
@@ -11,6 +32,14 @@ type Config struct {
 	RepositoryRootPath      string   // Absolute path to the root of the workspace.
 	BobWorkspaceRootRelPath string   // Relative module path to Bob workspace root.
 	BobIgnoreDir            []string // Relative path to ignore list from workspace root
+
+	IsIgnored bool
+	Mapper    *mapper.Mapper
+	Blueprint BlueprintConfig
+	Mconfig   MconfigConfig
+	Logic     LogicConfig            // Configuration for generating logical expressions.
+	Files     map[string]interface{} // Parsed file AST
+
 }
 
 func (c *Config) NewChild() *Config {
@@ -19,15 +48,42 @@ func (c *Config) NewChild() *Config {
 		RepositoryRootPath:      c.RepositoryRootPath,
 		BobWorkspaceRootRelPath: c.BobWorkspaceRootRelPath,
 		BobIgnoreDir:            c.BobIgnoreDir,
+		IsIgnored:               c.IsIgnored,
+
+		Blueprint: BlueprintConfig{
+			// Pass parent scope for blueprint parsing
+			Parser: bp.New(c.Mapper, c.Blueprint.Parser.GetScope()),
+
+			// Inherit other settings
+			Filenames: c.Blueprint.Filenames,
+			Builder:   c.Blueprint.Builder,
+		},
+		Mconfig: c.Mconfig,
+		Logic:   c.Logic,
+		Mapper:  c.Mapper,
+		Files:   map[string]interface{}{},
 	}
 }
 
 func NewRootConfig(repositoryRootPath string) *Config {
+	m := mapper.NewMapper()
+	lb := lb.New(m)
 	return &Config{
-		parent:                  nil,
-		RepositoryRootPath:      repositoryRootPath,
-		BobWorkspaceRootRelPath: "",
-		BobIgnoreDir:            nil,
+		RepositoryRootPath: repositoryRootPath,
+		Mconfig: MconfigConfig{
+			Filenames: []string{"Mconfig"}, //Default filenames to parse
+			Builder:   mb.NewBuilder(m, lb),
+			Parser:    mp.New(m),
+		},
+		Blueprint: BlueprintConfig{
+			Filenames: []string{"build.bp"}, //Default filenames to parse
+			Builder:   bb.NewBuilder(m, lb),
+			Parser:    bp.New(m, nil),
+		},
+		Logic:     LogicConfig{lb},
+		Files:     map[string]interface{}{},
+		Mapper:    m,
+		IsIgnored: false,
 	}
 }
 
