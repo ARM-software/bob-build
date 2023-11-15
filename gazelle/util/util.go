c7a1cf3d95be9b712c2fc3900578e2cc30cfee12
Lukasz Okraszewski <lukasz.okraszewski@arm.com>
2023-11-15 07:48:53 +0000

    refactor(gazelle): add result merger
    Change-Id: I28e358b3ccab445f03e4c665bb98f9e6a9f55974

--- gazelle/util/util.go
+++ gazelle/util/util.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/bazelbuild/bazel-gazelle/language"
 )
 
 func IsChildFilepath(parent string, child string) (bool, error) {
@@ -16,4 +18,13 @@ func IsChildFilepath(parent string, child string) (bool, error) {
 	}
 
 	return false, nil
+}
+
+func MergeResults(args ...language.GenerateResult) (merged language.GenerateResult) {
+	for _, r := range args {
+		merged.Gen = append(merged.Gen, r.Gen...)
+		merged.Empty = append(merged.Empty, r.Empty...)
+		merged.Imports = append(merged.Imports, r.Imports...)
+	}
+	return
 }
