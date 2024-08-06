f09a596f83ea12c3a5b6159189fdc726014a146c
Sebastian Birunt <sebastian.birunt@arm.com>
2024-08-06 11:00:51 +0200

    fix: broken `bzlmod`
    Fix unknown repo 'config_deps'.
    
    Change-Id: I6ce67dc5ea8c8057e35a4bd99f929e1bd849ec58
    Signed-off-by: Sebastian Birunt <sebastian.birunt@arm.com>

--- bpfmt_wrapper.sh
+++ bpfmt_wrapper.sh
@@ -4,4 +4,4 @@ WORK_DIR=$(pwd)
 if test "${BUILD_WORKING_DIRECTORY+x}"; then
   cd "$BUILD_WORKING_DIRECTORY"
 fi
-"$WORK_DIR"/bpfmt_/bpfmt "${@:1}"
+"$WORK_DIR"/external/com_github_google_blueprint/bpfmt/bpfmt_/bpfmt "${@:1}"
