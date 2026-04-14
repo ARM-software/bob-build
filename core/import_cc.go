6529b55ee1c03226b666e45758264b4f6f99b17c
Brandon Jones <brandon.jones@arm.com>
2026-04-14 10:15:18 +0100

    feat(import_cc): add header attribute
    Adds the header attribute and also symlinks them to the
    correct place in the generated tree to ensure that only
    listed headers can be included. Exporting on include directories
    is yet to be done
    
    Change-Id: I28eeade97a21f4b1bc78341b45d414802662ee87

--- core/import_cc.go
+++ core/import_cc.go
@@ -1,14 +1,18 @@
 package core
 
 import (
+	"path/filepath"
+
 	"github.com/ARM-software/bob-build/core/file"
 	"github.com/ARM-software/bob-build/core/module"
+	"github.com/ARM-software/bob-build/internal/utils"
 	"github.com/google/blueprint"
 )
 
 // TODO: Add Props one by one and test functionality of
 // headers, defines, `src` aka library, strip_include_prefix
 type ImportCCProps struct {
+	Headers []string
 }
 
 type ModuleImportCC struct {
@@ -24,6 +28,26 @@ type importCCInterface interface {
 	file.Provider
 }
 
+func (m *ModuleImportCC) shortName() string {
+	return m.Name()
+}
+
+func (m *ModuleImportCC) processPaths(ctx blueprint.BaseModuleContext) {
+	prefix := projectModuleDir(ctx)
+	m.Properties.Headers = utils.PrefixDirs(m.Properties.Headers, prefix)
+}
+
+func (m *ModuleImportCC) OutFiles() (files file.Paths) {
+	for _, h := range m.Properties.Headers {
+		src := file.NewPath(h, m.Name(), file.TypeHeader)
+		// `file.TypeGenerated` makes the file path exist under `$BUILDDIR/gen`
+		fp := file.NewLink(h, m.Name(), &src, file.TypeHeader|file.TypeGenerated)
+		files = append(files, fp)
+	}
+
+	return
+}
+
 func importCCFactory(config *BobConfig) (blueprint.Module, []interface{}) {
 	module := &ModuleImportCC{}
 
@@ -31,7 +55,27 @@ func importCCFactory(config *BobConfig) (blueprint.Module, []interface{}) {
 }
 
 func (g *linuxGenerator) importCCActions(m *ModuleImportCC, ctx blueprint.ModuleContext) {
+	installDeps := []string{}
+	m.OutFiles().ForEachIf(
+		func(fp file.Path) bool { return fp.IsSymLink() },
+		func(fp file.Path) bool {
+			if relative, err := filepath.Rel(fp.RelBuildPath(), fp.ExpandLink().RelBuildPath()); err == nil {
+				ctx.Build(pctx,
+					blueprint.BuildParams{
+						Rule:     symlinkRule,
+						Inputs:   []string{fp.ExpandLink().BuildPath()},
+						Outputs:  []string{fp.BuildPath()},
+						Args:     map[string]string{"target": relative},
+						Optional: true,
+					})
+				installDeps = append(installDeps, fp.BuildPath())
+				return true
+			}
 
+			return false
+		})
+
+	addPhony(m, ctx, installDeps, false) // Always add the symlinks
 }
 
 func (g *androidNinjaGenerator) importCCActions(m *ModuleImportCC, ctx blueprint.ModuleContext) {
